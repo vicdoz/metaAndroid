e428e5ebfc0f45da9ea2ce34f191eebed5997de9
delacrmi <delacrmi@gmail.com>
2014-05-13 10:26:58 +0200

    update manifest

---
+++ AndroidGenerator/src-gen/src/first/first.java
@@ -0,0 +1,15 @@
+
+	package first;
+	
+	import android.app.Activity;
+	import android.content.Intent;
+	import android.os.Bundle;
+	
+	public class first extends Activity {
+	@Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	         super.onCreate(savedInstanceState);
+	           
+	  }
+	
+	
---
+++ AndroidGenerator/src-gen/src/second/second.java
@@ -0,0 +1,15 @@
+
+	package second;
+	
+	import android.app.Activity;
+	import android.content.Intent;
+	import android.os.Bundle;
+	
+	public class second extends Activity {
+	@Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	         super.onCreate(savedInstanceState);
+	           
+	  }
+	
+	
