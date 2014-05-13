2afd8f79b90ef6e23b7ce81966caf3a6faadc323
Magnamarum <dasomar4@inf.upv.es>
2014-05-13 20:14:33 +0200

    "Cambios en manifest, ya tenemos permissions!!"

---
+++ AndroidGenerator/src-gen/src/null/null.java
@@ -0,0 +1,15 @@
+
+	package ;
+	
+	import android.app.Activity;
+	import android.content.Intent;
+	import android.os.Bundle;
+	
+	public class  extends Activity {
+	@Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	         super.onCreate(savedInstanceState);
+	           
+	  }
+	
+	
