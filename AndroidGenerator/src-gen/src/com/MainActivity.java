04ce4bdede61fa09421d8336d243e7277bb96be6
vicdoz <v.ahuir@gmail.com>
2014-05-15 10:31:16 +0200

    Menus generados

--- AndroidGenerator/src-gen/src/com/MainActivity.java
+++ AndroidGenerator/src-gen/src/com/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 @Override
   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.HORIZONTAL);
+		setContentView(R.layout.MainActivity);
   }
 
 }
