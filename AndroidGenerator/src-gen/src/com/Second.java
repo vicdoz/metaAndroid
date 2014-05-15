9054a95f762ad0fdf67637b9f13c636d60fe1d4a
vicdoz <v.ahuir@gmail.com>
2014-05-15 09:42:16 +0200

    Arreglados Strings. Regenerados plugins

---
+++ AndroidGenerator/src-gen/src/com/Second.java
@@ -0,0 +1,17 @@
+package com;
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Second extends Activity {
+@Override
+  protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.);
+  }
+
+}
+	
